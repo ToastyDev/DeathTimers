cd5953b5e04af83983388f0819cc2b79bb9a6bcf
Toasty <curtis.gaunt@uoit.com>
2018-08-28 22:33:05 -0400

    Began framework for slash commands Renamed original LUA file

---
+++ Death Timers/DeathTimers.lua
@@ -0,0 +1,7 @@
+SLASH_DEATHTIMERS1 = '/deathtimers'
+SLASH_DEATHTIMERS2 = '/dts'
+local total = 0
+
+SlashCmdList["DEATHTIMERS"] = function(self, txt)
+  SendChatMessage("Death Timers Options Open Here")
+end
