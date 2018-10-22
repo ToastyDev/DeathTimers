912ca106ced418e20419b4542512d00a88623550
Toasty <curtis.gaunt@uoit.com>
2018-10-21 22:29:52 -0400

    Added update calls for each text string reorganized OnEvent

--- Death Timers/DeathTimers.enUS.lua
+++ Death Timers/DeathTimers.enUS.lua
@@ -1,3 +1,5 @@
 DTLocalization = {}
 
 DTLocalization["ADDON_LOADED"] = "Death Timers Loaded"
+DTLocalization["Title_Text"] = "Death Timers"
+--Options localizaion text
--- Death Timers/DeathTimers.lua
+++ Death Timers/DeathTimers.lua
@@ -1,6 +1,10 @@
 local L = DTLocalization
 
-local total = 0
+local combatTime = 0
+local endTime = 0
+local totalElapsedTime = 0
+local timeLeft = 0
+local totalDPS = 0
 
 function DeathTimers_OnLoad(frame)
   frame:RegisterEvent("ADDON_LOADED")
@@ -10,25 +14,42 @@ end
 function DeathTimers_OnEvent(self, event, ...)
   if event == "ADDON_LOADED" then
     print(L.ADDON_LOADED)
+    DTFrameTitle:SetText(L.Title_Text)
     self:UnregisterEvent("ADDON_LOADED")
     --Register Combat Events
     self:RegisterEvent("PLAYER_REGEN_ENABLED")
     self:RegisterEvent("PLAYER_REGEN_DISABLED")
-  end
 
-  if event == "PLAYER_REGEN_DISABLED" then
+  elseif event == "PLAYER_REGEN_DISABLED" then
     print("Death Timers - Combat")
-  else if event == "PLAYER_REGEN_ENABLED" then
+    combatTime = GetTime()
+    TimerText_Update(combatTime)
+
+  elseif event == "PLAYER_REGEN_ENABLED" then
     print("Death Timers - No Combat")
+    endTime = GetTime()
   end
 end
+
+function DeathTimers_OnUpdate(self, elapsedTime)
+  totalElapsedTime = totalElapsedTime + elapsedTime
 end
 
+function CombatTimeText_Update()
+  DTFrameTimeLeft:SetText("Kill in: ")
+end
+
+function DPSText_Update()
+  DTFrameGrpDPS:SetText("Group DPS: ")
+end
+
+function TimerText_Update(time)
+  DTFrameCombatTime:SetText("Combat Time: " .. time)
+end
 
 SLASH_DEATHTIMERS1 = '/deathtimers'
 SLASH_DEATHTIMERS2 = '/dts'
 --Create slash commands to open options menu
 SlashCmdList["DEATHTIMERS"] = function(self, txt)
   DTOptionsFrame:Show()
-  print("DT Options")
 end
